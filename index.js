7691697258f1af58b4dfa264a04033d92b65195d
Darrell-Rafa <102950390+Darrell-Rafa@users.noreply.github.com>
2022-05-23 19:35:34 +0700

    update h1 gameOver

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function checkAnswer(currentLevel) {
         setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("#level-title").text("Game Over, Press or Click to Restart");
         startOver();
     }
 
